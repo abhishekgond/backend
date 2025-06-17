32629c6cfc90ce5a4b584e93217e02d23ad8abe2
Abhishek kumar <abhishekkumargond592@gmail.com>
2025-06-17 10:08:57 +0530

    ur; modifing

--- backend/index.js
+++ backend/index.js
@@ -245,7 +245,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   // console.log(`🚀 Server running on port ${PORT}`);
 });
